c2755b96004b2d773c4d8c5291092915137aa9ad
netlykos <16106232+netlykos@users.noreply.github.com>
2021-08-15 21:45:43 +0100

    Initial project setup

--- backend/app/src/main/java/org/netlykos/barcode/app/App.java
+++ backend/app/src/main/java/org/netlykos/barcode/app/App.java
@@ -2,12 +2,17 @@ package org.netlykos.barcode.app;
 
 import static org.netlykos.barcode.utilities.Greeting.greeting;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
 
-  private static final String GREETING_MESSAGE = "Hello World";
+  private static final String GREETING_MESSAGE = "Hello World!";
 
   public static void main(String[] args) {
     System.out.println(greeting(GREETING_MESSAGE));
+    SpringApplication.run(App.class, args);
   }
 
 }
